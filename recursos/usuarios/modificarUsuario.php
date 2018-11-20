4372973b13d0c69c3dab4b95bf936dc1ed83b6e3
jaunsdw <faltron@misena.edu.co>
2018-11-20 09:35:51 -0500

    Update modificarUsuario.php

--- recursos/usuarios/modificarUsuario.php
+++ recursos/usuarios/modificarUsuario.php
@@ -9,25 +9,79 @@
             $sql="UPDATE usuarios  
                     SET Password = $NuevaClave
                     WHERE Usuario = $NomUsuario"; 
+
+
+            $resultado = cambiarContraseña($miConexion,$sql);
+            $respuesta->preparar($resultado['Codigo'],$resultado['Mensaje']);
+            $respuesta->responder();
+
         }else {
-            $sql="UPDATE usuarios  
-                    SET Password = $NuevaClave
-                    WHERE Id = $IdUsuario"; 
+
+            $autorizacion = verificarContraseña($viejaClave,$miConexion,$IdUsuario);
+
+            if($autorizacion != "ok"){
+                $respuesta->preparar($autorizacion['Codigo'],$autorizacion['Mensaje']);
+                $respuesta->responder();
+            }else {
+
+                $sql="UPDATE usuarios  
+                SET Password = $NuevaClave
+                WHERE Id = $IdUsuario";
+
+                $resultado = cambiarContraseña($miConexion,$sql);
+                $respuesta->preparar($resultado['Codigo'],$resultado['Mensaje']);
+                $respuesta->responder();
+
+            }
+
+ 
         }
+    }
+
+
+
+
+    function verificarContraseña($viejaClave,$miConexion,$usuario){
+
+        $sql="SELECT * FROM usuarios WHERE Id = $usuario";
+        
+        $miConexion->EjecutarSQL($sql);
+      
+
+        if ($miConexion->GetCodigoRespuesta() == 400){
+            $error = $miConexion->GetError();
+            return array("Codigo"=>400,"Mensaje"=>"Error al consultar ($sql)".$error);
+        }else{
+
+            $resultado = $miConexion->GetResultados();
+            
+
+
+            if($viejaClave == $resultado[0]['Password']){
+                return "ok";
+            }else {
+                return array("Codigo"=>404,"Mensaje"=>"Password incorrecto");
+            }
+
+        }        
+        
+    }
+
+    function cambiarContraseña($miConexion,$sql){
 
         $miConexion->EjecutarSQL($sql);
         
         if ($miConexion->GetCodigoRespuesta() == 400){
             $error = $miConexion->GetError();
-            $respuesta->preparar(400, "Error al consultar ($sql)".$error);
+            return array("Codigo"=>400,"Mensaje"=>"Error al consultar ($sql)".$error);
         }else{
 
             $resultado = $miConexion->ConsultarModificaciones();
-            $respuesta->preparar(200,"Filas modificadas ".$resultado);
- 
+            return array("Codigo"=>200,"Mensaje"=>"Filas modificadas ".$resultado);
         }
     }
-
-    $respuesta->responder();
+        
+ 
+    
 
 ?>
