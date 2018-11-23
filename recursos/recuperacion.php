38c8b3c10deacbe87c2c07c2958fa5c1f92be37b
jaunsdw <faltron@misena.edu.co>
2018-11-22 23:43:41 -0500

    Update recuperacion.php

--- recursos/recuperacion.php
+++ recursos/recuperacion.php
@@ -1,7 +1,7 @@
 <?php
     header("Access-Control-Allow-Origin:*");
     header("Access-Control-Allow-Headers:*");
-    header("Access-Control-Allow-Methods:*");
+    header("Access-Control-Allow-Methods: POST");
 
     $post_vars=file_get_contents("php://input"); // Extracción de datos
     $post_vars= json_decode($post_vars,true); // Descodificacion de json 
@@ -17,70 +17,79 @@
     $respuesta = new ControlRespuesta($miConexion); // instancia de la clase ControlRespuesta
     $miConexion->Conectar(); // Metodo que ejecuta la conexion con la base de datos
 
- 
-    if(!(isset($Codigo))){
-   
-        $validar = validarUsuario($Usuario,$miConexion);
-        switch ($validar) {
-            case '503':
-                $respuesta->preparar(503,"Servicio No disponible BD");
-                $respuesta->responder();
-                break;
-            case '400':
-                $error = $miConexion->GetError();
-                $respuesta->preparar(400, "Error al consultar ($sql)".$error);
-                $respuesta->responder();
-                break;    
-            case 'empty':
-                $respuesta->preparar(200, "Usuario no existe");
-                $respuesta->responder();
-                break;
-            default:
-                require_once("usuarios/enviarCorreo.php");
-                break;
-        }
+        
+    $metodo = $_SERVER['REQUEST_METHOD'];
 
-    }elseif($Codigo == 'nuevaClave'){
-
-        require_once("usuarios/modificarUsuario.php");
-
-    }
-    else{
-
-        $validar = consultarCodigo($Usuario,$miConexion);
-
-
-        switch ($validar) {
-            case '503':
-                $respuesta->preparar(503,"Servicio No disponible BD");
-                $respuesta->responder();
-                break;
-            case '400':
-                $error = $miConexion->GetError();
-                $respuesta->preparar(400, "Error al consultar ($sql)".$error);
-                $respuesta->responder();
-                break;    
-            case 'empty':
-                $respuesta->preparar(200, "No existe codigo registrado");
-                $respuesta->responder();
-                break;
-            case 'NULL':
-                $respuesta->preparar(200, "No existe codigo registrado");
-                $respuesta->responder();
-            default:
-                
-                if (!($Codigo == $validar)){
-                    $respuesta->preparar(200, "El codigo es incorrecto");
+    if($metodo != 'POST'){
+            if($metodo != 'OPTIONS'){
+                    $respuesta->preparar(403,"Llamado Erroneo");
                     $respuesta->responder();
-                }else {
-                    $respuesta->preparar(200, "El codigo es correcto");
+            }
+    }else{
+        if(!(isset($Codigo))){
+    
+            $validar = validarUsuario($NomUsuario,$miConexion);
+            switch ($validar) {
+                case '503':
+                    $respuesta->preparar(503,"Servicio No disponible BD");
                     $respuesta->responder();
-                }
-                break;
+                    break;
+                case '400':
+                    $error = $miConexion->GetError();
+                    $respuesta->preparar(400, "Error al consultar ($sql)".$error);
+                    $respuesta->responder();
+                    break;    
+                case 'empty':
+                    $respuesta->preparar(200, "Usuario no existe");
+                    $respuesta->responder();
+                    break;
+                default:
+                    require_once("usuarios/enviarCorreo.php");
+                    break;
             }
 
-        
+        }elseif($Codigo == 'nuevaClave'){
 
+            require_once("usuarios/modificarUsuario.php");
+
+        }
+        else{
+
+            $validar = consultarCodigo($NomUsuario,$miConexion);
+
+
+            switch ($validar) {
+                case '503':
+                    $respuesta->preparar(503,"Servicio No disponible BD");
+                    $respuesta->responder();
+                    break;
+                case '400':
+                    $error = $miConexion->GetError();
+                    $respuesta->preparar(400, "Error al consultar ($sql)".$error);
+                    $respuesta->responder();
+                    break;    
+                case 'empty':
+                    $respuesta->preparar(200, "No existe codigo registrado");
+                    $respuesta->responder();
+                    break;
+                case 'NULL':
+                    $respuesta->preparar(200, "No existe codigo registrado");
+                    $respuesta->responder();
+                default:
+                    
+                    if (!($Codigo == $validar)){
+                        $respuesta->preparar(200, "El codigo es incorrecto");
+                        $respuesta->responder();
+                    }else {
+                        $respuesta->preparar(200, "El codigo es correcto");
+                        $respuesta->responder();
+                    }
+                    break;
+                }
+
+            
+
+        }
     }
 
     function consultarCodigo($NomUsuario,$miConexion){
