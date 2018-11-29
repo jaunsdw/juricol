8b1451ca903b2135bf874e59b3f60b2334b0461b
jaunsdw <faltron@misena.edu.co>
2018-11-29 14:31:42 -0500

    Update consultarDemandas.php

--- recursos/demandas/consultarDemandas.php
+++ recursos/demandas/consultarDemandas.php
@@ -5,30 +5,46 @@
             
         if(!(isset($Filtro)) || empty($Filtro)){
             $sql="SELECT 
-                        D.Id AS 'IdDemanda',
-                        D.Clientes_id AS 'IdCliente',
-                        CONCAT(C.PrimerNombre,' ',C.SegundoNombre,' ',C.PrimerApellido,' ',C.SegundoApellido) as 'NombreCliente',
-                        D.NumDemanda AS 'NumDemanda',
-                        D.TiposDemandas_id AS 'IdTipoDemanda',
-                        TPS.Descripcion AS 'NombreTipoDemanda',
-                        D.Titular_id AS 'IdTitular',
-                        CONCAT(E1.PrimerNombre,' ',E1.SegundoNombre,' ',E1.PrimerApellido,' ',E1.SegundoApellido) as 'NombreTitular',
-                        D.EstadosProcesos_id AS 'IdEstadoProceso',
-                        ESP.Descripcion AS 'NombreEstadoProceso',
-                        IF(D.Categoria = 1,'Ataque','Defensa') AS 'Categoria',
-
-                        D.FechaCreacion AS 'FechaCreacion'
-                    FROM demandas AS D
-                        INNER JOIN clientes as C
-                            ON C.Id = D.Clientes_id
-                        INNER JOIN tiposdemandas AS TPS
-                            ON	TPS.Id = D.TiposDemandas_id
-                        INNER JOIN empleados as E1 
-                            ON E1.Id = D.Titular_id
-                        INNER JOIN empleados AS E2
-                            ON E2.Id = D.Suplente_id
-                        INNER JOIN estadosprocesos AS ESP
-                            ON ESP.Id = D.EstadosProcesos_id";
+                    D.Id AS 'IdDemanda',
+                    D.Clientes_id AS 'IdCliente',
+                    CONCAT(C.PrimerNombre,' ',C.SegundoNombre,' ',C.PrimerApellido,' ',C.SegundoApellido) as 'NombreCliente',
+                    D.NumDemanda AS 'NumDemanda',
+                    D.TiposDemandas_id AS 'IdTipoDemanda',
+                    TPS.Descripcion AS 'NombreTipoDemanda',
+                    D.Titular_id AS 'IdTitular',
+                    CONCAT(E1.PrimerNombre,' ',E1.SegundoNombre,' ',E1.PrimerApellido,' ',E1.SegundoApellido) as 'NombreTitular',
+                    D.EstadosProcesos_id AS 'IdEstadoProceso',
+                    ESP.Descripcion AS 'NombreEstadoProceso',
+                    D.FechaCreacion AS 'FechaCreacion',
+                    (SELECT  ED.Descripcion
+                        FROM movimientos AS M
+                            INNER JOIN estadosdemandas AS ED
+                                ON M.EstadosDemandas_id = ED.Id
+                        WHERE FechaMovimiento = (SELECT  max(FechaMovimiento)
+                                                                    FROM movimientos 
+                                                                    WHERE Demandas_id = D.Id) && M.Demandas_id = D.Id) AS 'UltimoMovimiento',
+                    (SELECT  FechaMovimiento
+                        FROM movimientos 
+                        WHERE FechaMovimiento = (SELECT  max(FechaMovimiento)
+                                                                    FROM movimientos 
+                                                                    WHERE Demandas_id = D.Id) && Demandas_id = D.Id) AS 'FechaMovimiento',
+                    (SELECT  FechaLimite
+                        FROM movimientos 
+                        WHERE FechaMovimiento = (SELECT  max(FechaMovimiento)
+                                                                    FROM movimientos 
+                                                                    WHERE Demandas_id = D.Id) && Demandas_id = D.Id) AS 'FechaLimite'
+            
+                FROM demandas AS D
+                    INNER JOIN clientes as C
+                        ON C.Id = D.Clientes_id
+                    INNER JOIN tiposdemandas AS TPS
+                        ON	TPS.Id = D.TiposDemandas_id
+                    INNER JOIN empleados as E1 
+                        ON E1.Id = D.Titular_id
+                    INNER JOIN empleados AS E2
+                        ON E2.Id = D.Suplente_id
+                    INNER JOIN estadosprocesos AS ESP
+                        ON ESP.Id = D.EstadosProcesos_id";
         }else {
 
             switch ($Filtro) {
