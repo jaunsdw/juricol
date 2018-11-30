ad6a2b923e3fe7f06a3dee920582135000d6c66e
jaunsdw <faltron@misena.edu.co>
2018-11-30 02:49:18 -0500

    1

---
+++ recursos/empleados/cambioEstado.php
@@ -0,0 +1,54 @@
+<?php
+        if ($miConexion->GetCodigoRespuesta() == 503) {  
+            $respuesta->preparar(503, "Servicio No disponible BD");
+        }
+        else{              
+            
+            $estadoActual = verificarEstadoActual($IdEmpleado,$miConexion);
+
+            var_dump($estadoActual);
+            exit();
+        
+            $SQL = "UPDATE empleados  
+                           SET Estados = '$nuevoEstado'
+                        WHERE Id = $IdEmpleado";
+ 
+            $miConexion->EjecutarSQL($SQL); 
+
+            if ( $miConexion->GetCodigoRespuesta() == 400 ) {   
+                $error = $miConexion->GetError();
+                $respuesta->preparar(400, "Error al consultar (".$SQL.") ".$error);
+                
+            }
+            else{                                      
+
+                $respuesta->preparar(200,$miConexion->ConsultarModificaciones());
+                
+            }
+
+        }
+
+    $respuesta->responder();
+
+
+
+
+
+
+    function verificarEstadoActual($IdEmpleado,$miConexion){
+
+        $SQL = "SELECT Estados FROM empleados WHERE Id = $IdEmpleado";
+ 
+        $miConexion->EjecutarSQL($SQL); 
+
+        if ( $miConexion->GetCodigoRespuesta() == 400 ) {   
+            $error = $miConexion->GetError();
+            $respuesta->preparar(400, "Error al consultar (".$SQL.") ".$error);
+            
+        }else {
+            $resultado = $miConexion->GetResultados();
+            $estadoActual = $resultado[0]['Estados'];
+            return $estadoActual;
+        }
+    }
+?>
