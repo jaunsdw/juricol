a76ff47c80ee434b49bc49be7922402752206e80
jaunsdw <faltron@misena.edu.co>
2018-11-30 04:00:32 -0500

    Update consultarMovimientos.php

--- recursos/movimientos/consultarMovimientos.php
+++ recursos/movimientos/consultarMovimientos.php
@@ -8,7 +8,15 @@
             $respuesta->preparar(400, "No se entrego IdDemanda");
             $respuesta->responder();
          }else {
-             $sql="SELECT ";
+             $sql="SELECT ED.Descripcion as 'NombreEstado',
+                            M.FechaMovimiento as 'FechaMovimiento',
+                            M.Descripcion as 'Descripcion',
+                            M.FechaLimite as 'FechaLimite' 
+                        FROM movimientos AS M
+                            INNER JOIN estadosdemandas AS ED
+                                ON M.EstadosDemandas_id = ED.Id
+                        WHERE M.Demandas_id = $IdDemanda
+                        ORDER BY FechaMovimiento DESC ";
          }
 
         $miConexion->EjecutarSQL($sql);
