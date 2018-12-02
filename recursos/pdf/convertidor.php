146ac56f1a93ebb6f8ac623e86e5f094c5d8448b
jaunsdw <faltron@misena.edu.co>
2018-12-01 20:40:22 -0500

    Update convertidor.php

--- recursos/pdf/convertidor.php
+++ recursos/pdf/convertidor.php
@@ -100,13 +100,13 @@
 
                                     
                                         $error = $miConexion->GetError();
-                                        $respuesta->preparar(400, "Error al consultar ($sql)".$error);
+                                        var_dump(400, "Error al consultar ($sql)".$error);
 
 
                                     }else{
 
                                         $resultado = $miConexion->GetResultados();
-                                        $respuesta->preparar(200, $resultado);
+                                        var_dump(200, $resultado);
                             
                                     }
 
@@ -187,13 +187,13 @@
 
                                             
                                                 $error = $miConexion->GetError();
-                                                $respuesta->preparar(400, "Error al consultar ($sql)".$error);
+                                                var_dump(400, "Error al consultar ($sql)".$error);
 
 
                                             }else{
 
                                                 $resultado = $miConexion->GetResultados();
-                                                $respuesta->preparar(200, $resultado);
+                                                var_dump(200, $resultado);
                                     
                                             }
 
