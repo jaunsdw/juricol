e8422618b93ed7c59001250659bff897363189d0
jaunsdw <faltron@misena.edu.co>
2018-12-04 07:24:54 -0500

    Update validar.php

--- recursos/validar.php
+++ recursos/validar.php
@@ -1,7 +1,7 @@
 <?php
     header("Access-Control-Allow-Origin:*");
     header("Access-Control-Allow-Headers:*");
-    header("Access-Control-Allow-Methods: *");
+    header("Access-Control-Allow-Methods: POST,PUT,GET,DELETE");
 
     
     if($_SERVER['REQUEST_METHOD'] == "OPTIONS"){
