a3625282ee9e64875b3404d56e8a1cc9434f4eee
jaunsdw <faltron@misena.edu.co>
2018-12-04 11:11:01 -0500

    Update crearMovimiento.php

--- recursos/movimientos/crearMovimiento.php
+++ recursos/movimientos/crearMovimiento.php
@@ -55,38 +55,54 @@ if ($miConexion->GetCodigoRespuesta() == 503 ){
                 $respuesta->responder();  
 
             } else {
-                      
-                $sql="INSERT INTO movimientos ( Demandas_id,
-                                EstadosDemandas_id,
-                                FechaMovimiento,
-                                Descripcion,
-                                FechaLimite)
-                        VALUES ($IdDemanda,
-                                $IdEstadoDemanda,
-                                '$FechaMovimiento',
-                                '$Descripcion',
-                                '$FechaLimite')";
 
-                    $miConexion->EjecutarSQL($sql);
+                $existencia = verificarFecha($IdDemanda,$FechaMovimiento,$miConexion);
 
-                    if ($miConexion->GetCodigoRespuesta() == 400){
-                    $error = $miConexion->GetError();
-                    $respuesta->preparar(400, "Error al consultar ($sql)".$error);
-                    }else{
+                if ($existencia != "ok") {
+                    $respuesta->preparar(403,$existencia);
+                    $respuesta->responder(); 
+                } else {
+                    $sql="INSERT INTO movimientos ( Demandas_id,
+                                        EstadosDemandas_id,
+                                        FechaMovimiento,
+                                        Descripcion,
+                                        FechaLimite)
+                                VALUES ($IdDemanda,
+                                        $IdEstadoDemanda,
+                                        '$FechaMovimiento',
+                                        '$Descripcion',
+                                        '$FechaLimite')";
 
-                    $resultado = $miConexion->ConsultarIdInsertado();
-                    $respuesta->preparar(200,"Id insertado ".$resultado);
+                            $miConexion->EjecutarSQL($sql);
 
-                    }
+                            if ($miConexion->GetCodigoRespuesta() == 400){
+                                $error = $miConexion->GetError();
+                                $respuesta->preparar(400, "Error al consultar ($sql)".$error);
+                            }else{
+                                $resultado = $miConexion->ConsultarIdInsertado();
+                                $respuesta->preparar(200,"Id insertado ".$resultado);
+                            }
 
                     $respuesta->responder();  
+                }
             }
-            
-
         }
     }
 
+    function verificarFecha($IdDemanda,$FechaMovimiento,$miConexion){
 
+        $sql="SELECT * 
+            FROM movimientos
+            WHERE Demandas_id = $IdDemanda AND FechaMovimiento = $FechaMovimiento ";
+
+        $miConexion->EjecutarSQL($sql);
+        $resultado = $miConexion->GetResultados();
+        if (empty($resultado)) {
+            return "ok";
+        } else {
+            return "Esta demanda ya tiene un movimiento registrado en esta fecha";
+        }
+    }
 
     function verificarExistencia($IdDemanda,$IdEstadoDemanda,$miConexion){
 
